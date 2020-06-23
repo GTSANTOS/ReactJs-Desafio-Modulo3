5b53d8a425556030bd88f81d7212f87eb1e325e0
Gabriel <gabriel.santos@logplex.com.br>
2020-06-23 12:26:28 -0300

    Ajustes no package.json

---
+++ build/precache-manifest.3d8e164840e807f884a2abc4ac40ac0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae3905ff544db29ff67749d467aaad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4ed97a74f56440eacb2",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "fbdd4db644d103a49cb2",
+    "url": "/static/css/main.d9a21b28.chunk.css"
+  },
+  {
+    "revision": "e4ed97a74f56440eacb2",
+    "url": "/static/js/2.95c54e45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95c54e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbdd4db644d103a49cb2",
+    "url": "/static/js/main.3840b012.chunk.js"
+  },
+  {
+    "revision": "67e85333bb14618f3a58",
+    "url": "/static/js/runtime-main.d0c8e979.js"
+  }
+]);
